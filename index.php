8967c2709a2bf02380ad1d94d9b6696b140246eb
hamidoucheasmae <hamidouche.asmae@gmail.com>
2021-05-07 11:14:39 +0000

    EN COURS

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
 
-phpinfo();
+  // Ouverture de la connexion à la base de données
+  $dbhost = 'localhost:3306';
+  $dbuser = 'root';
+  $dbpass = '';
+  $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+  
+
+if(! $conn ) {
+echo "not connected :";
+exit();
+}
+echo "Connected successfully";
 
 ?>
---
+++ index1.php
@@ -0,0 +1,23 @@
+<?php
+   $dbhost = 'localhost:3306';
+   $dbuser = 'root';
+   $dbpass = '';
+   $dbname = 'first';
+   
+   $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+   if ($mysqli->connect_errno) {
+      echo "Échec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+   }
+
+   $res = $mysqli->query("SELECT * FROM first.offices");
+
+
+   $res->data_seek(0);
+   while ($row = $res->fetch_assoc()) {
+      echo " id = " . $row['Id'] . "\n";
+      echo " Price = " . $row['Price'] . "\n";
+      echo " Title = " . $row['Title'] . "\n";
+   }
+
+
+?>
