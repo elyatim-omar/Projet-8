badaeb6e7c67f9d009c07d3990d24e7b2e06f08a
hamidoucheasmae <hamidouche.asmae@gmail.com>
2021-06-03 16:06:43 +0100

    modification

--- realisation/crud-ajax-json-jquery-php-master/api/deleteStudent.php
+++
@@ -1,7 +0,0 @@
-<?php
-$id = $_POST["sid"];
-$dbh = new PDO("mysql:host=localhost;dbname=schoolstd", "root", "solicode123");
-$sql = "DELETE FROM schoolstudents WHERE id = $id";
-$getStudents = $dbh->prepare($sql) ;
-$getStudents->execute();
-?>
