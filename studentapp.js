91513e7b233a98b189d76e491e3105bc6ecfd241
hamidoucheasmae <hamidouche.asmae@gmail.com>
2021-06-04 10:00:29 +0100

    modification

--- studentapp.js
+++ studentapp.js
@@ -9,7 +9,7 @@ function getall() {
       studentsList = JSON.parse(data);
       console.log(studentsList);
       for (var i = 0; i < studentsList.length; i++) {
-        $(".studentsList").append('<li><h2>'+"Name: "+studentsList[i]["name"]+'</h2><h3>'+"FatherName: "+studentsList[i]["fathername"]+'</h3><h4>'+"Rollno: "+studentsList[i]["rollno"]+'</h4><h5>'+"Degree: "+studentsList[i]["degree"]+'</h5><h6>'+"Branch: "+studentsList[i]["branch"]+'</h6></li>');
+        $(".studentsList").append('<li><h2>'+"Name: "+studentsList[i]["name"]+'</h2><h3>'+"FatherName: "+studentsList[i]["fathername"]+'</h3><h4>'+"position: "+studentsList[i]["position"]+'</h4><h5>'+'</h5><h6></li>');
       }
     }
   })
